610fbdbbee0f2f20643bd7b62828d55e480c2cfe
EGA-SUPREMO <cxiesestro@gmail.com>
2020-02-18 21:47:18 -0430

    add ResultWordList component

---
+++ src/app/result-word-list/result-word-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-word-list',
+  templateUrl: './result-word-list.component.html',
+  styleUrls: ['./result-word-list.component.css']
+})
+export class ResultWordListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
