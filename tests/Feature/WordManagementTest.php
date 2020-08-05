441dac6c87d8c59270938ae8d814af958a02d68a
EGA-SUPREMO <cxiesestro@gmail.com>
2020-08-05 16:25:32 -0400

    add tests signed_user_can_practice & unsigned not

--- tests/Feature/WordManagementTest.php
+++ tests/Feature/WordManagementTest.php
@@ -6,12 +6,31 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\User;
+
 class WordManagementTest extends TestCase
 {
-    public function testExample()
+    use RefreshDatabase;
+
+    protected function setUp(): void
     {
+        parent::setUp();
+        $this->withoutExceptionHandling();
+    }
+    
+    public function test_signed_user_can_practice()
+    {
+        $this->actingAs(factory(User::class)->create());
+
         $response = $this->get('/practice');
 
         $response->assertOk();
+    }
+    public function test_unsigned_user_cannot_practice()
+    {
+        $this->withExceptionHandling();
+        $response = $this->get('/practice');
+
+        $response->assertRedirect('/login');
     }
 }
